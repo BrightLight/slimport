7667ee37bca36c819bfa503457c2134024f4cf3c
BrightLight <ml@silverseed.de>
2019-05-03 14:14:30 +0200

    Add logging

---
+++ source/ConsoleLogger.cs
@@ -0,0 +1,49 @@
+﻿namespace slimport
+{
+  using System;
+
+  public class ConsoleLogger : ILogger
+  {
+    private string context;
+
+    public void Update(string name, string oldValue, string newValue)
+    {
+      if (string.IsNullOrEmpty(oldValue))
+      {
+        Console.WriteLine($"{this.context}: Add {name}: [{newValue}]");
+      }
+      else
+      {
+        Console.WriteLine($"{this.context}: Update {name} from [{oldValue}] to [{newValue}]");
+      }
+    }
+
+    public void Delete(string name)
+    {
+      Console.WriteLine($"{this.context}: Delete {name}");
+    }
+
+    public void SetContext(string context)
+    {
+      this.context = context;
+    }
+  }
+
+  public class XmlLogger : ILogger
+  {
+    public void Update(string name, string oldValue, string newValue)
+    {
+      // TODO_IMPLEMENT_ME();
+    }
+
+    public void Delete(string name)
+    {
+      // TODO_IMPLEMENT_ME();
+    }
+
+    public void SetContext(string context)
+    {
+      // TODO_IMPLEMENT_ME();
+    }
+  }
+}
---
+++ source/ILogger.cs
@@ -0,0 +1,9 @@
+﻿namespace slimport
+{
+  public interface ILogger
+  {
+    void Update(string name, string oldValue, string newValue);
+    void Delete(string name);
+    void SetContext(string context);
+  }
+}
